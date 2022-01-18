4a7fb6708ab388f1c4c1435061e5516ed7a1648e
juanmi <jmcabrera@copado.com>
2022-01-18 14:44:32 +0100

    [node] Check existing user

--- node/src/routes/authRouter.js
+++ node/src/routes/authRouter.js
@@ -5,6 +5,7 @@ import { toResponse as toResponseComment } from '../models/comment.js';
 import mongoose from 'mongoose';
 
 const NOT_FOUND_CODE = 400;
+const INTERNAL_ERROR_CODE = 500;
 
 const USERNAME_REQUIRED_RESPONSE = { "error": "User name is mandatory." }
 const EMAIL_REQUIRED_RESPONSE = { "error": "Email is mandatory." }
@@ -13,22 +14,18 @@ const ROLE_REQUIRED_RESPONSE = { "error": "Role not found." }
 
 const router = Router();
 
-router.post('/signup', async (req, res) => {
+router.post('/signup',async (req, res) => {
 
-    if (!req.body.username) {
-        console.log(USERNAME_REQUIRED_RESPONSE);
-        return res.status(NOT_FOUND_CODE).send(USERNAME_REQUIRED_RESPONSE);
+    let verifyResult = verifyRequest(req);
+    if (verifyResult) {
+        return res.status(NOT_FOUND_CODE).send(verifyResult);
     }
 
-    if (!req.body.email) {
-        console.log(EMAIL_REQUIRED_RESPONSE);
-        return res.status(NOT_FOUND_CODE).send(EMAIL_REQUIRED_RESPONSE);
-    }
-
-    if (!req.body.password) {
-        console.log(PASS_REQUIRED_RESPONSE);
-        return res.status(NOT_FOUND_CODE).send(PASS_REQUIRED_RESPONSE);
-    }
+    await User.findOne({nick: req.body.username})
+        .exec((err, userDB) => {
+            if (err) return res.status(INTERNAL_ERROR_CODE).send({message: err});
+            if (userDB) return res.status(NOT_FOUND_CODE).send({ message: "Failed! Username is already in use!" });
+        });
 
     const roleDB = await Role.findOne({name: req.body.roles});
     if (!roleDB) {
@@ -49,8 +46,24 @@ router.post('/signup', async (req, res) => {
             console.log(error);
             res.status(400).send(error);
         });
-
 });
 
+function verifyRequest(req) {
+
+    if (!req.body.username) {
+        console.log(USERNAME_REQUIRED_RESPONSE);
+        return USERNAME_REQUIRED_RESPONSE;
+    }
+
+    if (!req.body.email) {
+        console.log(EMAIL_REQUIRED_RESPONSE);
+        return EMAIL_REQUIRED_RESPONSE;
+    }
+
+    if (!req.body.password) {
+        console.log(PASS_REQUIRED_RESPONSE);
+        return PASS_REQUIRED_RESPONSE;
+    }
+}
 
 export default router;
